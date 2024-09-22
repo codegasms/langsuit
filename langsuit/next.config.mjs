dadeada86ae2936fb7fbb7a40eb5270b38cf98c7
CS-parth <parth.vijy@gmail.com>
2024-09-22 21:01:06 +0530

    [Fix] [Done] [Parth] Fixed the lazy loading of environment variables causing neon() to take empty string as input

--- langsuit/next.config.mjs
+++ langsuit/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+import { config } from 'dotenv'; // Load environment variables from .env file
+config();
+
 const nextConfig = {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL, // Expose the variable to the client side if needed
+    },
     api: {
         bodyParser: {
           sizeLimit: '500kb',
