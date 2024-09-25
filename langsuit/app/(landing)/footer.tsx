6779658b2b87153ec1586e05f409123125aa7325
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-09-26 03:14:47 +0530

    [Add] [Done] [Bishwajeet] Add authentication with Clerk

--- langsuit/app/(landing)/footer.tsx
+++ langsuit/app/(landing)/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
-    return (
-        <footer className="hidden lg:block h-20 w-full border-t-2 bg-slate-200 p-2">
-            Footer
-        </footer>
-    );
+  return (
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      Footer
+    </footer>
+  );
 };
