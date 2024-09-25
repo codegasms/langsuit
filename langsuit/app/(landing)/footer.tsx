d208c3e0aa86b21e96ee48f8d8105ae739d195bf
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-09-26 01:26:51 +0530

    [Init] [Done] [Bishwajeet] Init landing page structure

---
+++ langsuit/app/(landing)/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="hidden lg:block h-20 w-full border-t-2 bg-slate-200 p-2">
+            Footer
+        </footer>
+    );
+};
---
+++ langsuit/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+          {/* <h1 className="text-green-500 font-bold">Welcome to Langsuit</h1> */}
+          This Is The Landing Page
+    </div>
+  );
+}
