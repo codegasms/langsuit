b853e11f010e45379c3e5acab224c1cf7fe60d02
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-09-26 23:52:57 +0530

    [Add] [Done] [Bishwajeet] Add Home Page (with static content as of now)

--- langsuit/app/(home)/learn/page.tsx
+++ langsuit/app/(home)/learn/page.tsx
@@ -1,7 +1,25 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { Header } from "./header";
+
 const LearnPage = () => {
   return (
-    <div>
-      Hello World
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{
+            title: "English",
+            imageSrc: "/US - United States.svg",
+          }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="English" />
+      </FeedWrapper>
     </div>
   );
 };
