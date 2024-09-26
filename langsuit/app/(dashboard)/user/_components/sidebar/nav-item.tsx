61153ffe1b52d1a35dc101403f215e53f57f1eb4
AkshatM1707 <akshatm2004@gmail.com>
2024-09-27 01:55:25 +0530

    [Add] [Done] [Parth] Added common components for the dashboard layout

---
+++ langsuit/app/(dashboard)/user/_components/sidebar/nav-item.tsx
@@ -0,0 +1,49 @@
+"use-client";
+
+import { LucideIcon } from "lucide-react";
+import { useInstructorSidebar } from '@/store/use-instructorsidebar';
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+interface navItemProps {
+    icon: LucideIcon;
+    label: string;
+    href: string;
+    isActive: boolean;
+};
+
+export const NavItem = ({
+    icon:Icon,
+    label,
+    href,
+    isActive
+}: navItemProps) => {
+    const { collapsed } = useInstructorSidebar((state) => state);
+    return (
+        <Button
+            asChild
+            variant={"ghost"}
+            className={cn(
+                "w-full h-12",
+                collapsed ? "justify-center" : "justify-start",
+                isActive && "bg-accent"
+            )}
+        >
+            <Link href={href}>
+                <div className="flex items-center gap-x-4">
+                    <Icon className={cn(
+                        "h-4 w-4",
+                        collapsed ? "mr-0" : "mr-2"
+                    )}>        
+                        {!collapsed && (
+                            <span>
+                                {label}
+                            </span>
+                        )}
+                    </Icon>
+                </div>
+            </Link>
+        </Button>
+    );
+}
