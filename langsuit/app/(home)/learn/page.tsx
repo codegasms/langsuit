6e018d2e80906de2d5dcd5348bb7b7fcdec0ef60
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-09-27 03:50:30 +0530

    [Add] [Done] [Bishwajeet] Add courses page

--- langsuit/app/(home)/learn/page.tsx
+++ langsuit/app/(home)/learn/page.tsx
@@ -3,7 +3,18 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { Header } from "./header";
 
-const LearnPage = () => {
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+   }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
