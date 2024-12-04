f87b66025ded54370246f7b94c5ebc883b489625
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-12-04 22:00:25 +0530

    [Add] [Done] [Bishwajeet] Admin CRUD functionality for courses with React Admin integration

---
+++ langsuit/app/admincrud/app.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import simpleRestProvider from "ra-data-simple-rest";
+import { Admin, Resource } from "react-admin";
+import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
+import { CourseList } from "./course/list";
+
+const dataProvider = simpleRestProvider("/api");
+
+const App = () => {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="courses"
+        recordRepresentation="title"
+        list={CourseList}
+        create={CourseCreate}
+        edit={CourseEdit}
+      />
+    </Admin>
+  );
+};
+
+export default App;
--- langsuit/app/admincrud/page.tsx
+++ langsuit/app/admincrud/page.tsx
@@ -1,10 +1,19 @@
 import dynamic from "next/dynamic";
 
+import { getIsAdmin } from "@/lib/admin";
+import { redirect } from "next/navigation";
+
 const App = dynamic(() => import("./app"), { ssr: false });
 
 const AdminCrudPage = () => {
+  const isAdmin = getIsAdmin();
+
+  if (!isAdmin) redirect("/");
+
   return (
-    <App />
+    <div>
+      <App />
+    </div>
   );
 };
 
