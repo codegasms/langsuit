63803ec40b83cfcebdc8bae56a26836640f61fa7
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-12-05 01:19:52 +0530

    [Fix] [Done] [Bishwajeet] Refactored CoursesPage to use real database queries and added ShopPage with maintenance message

--- langsuit/app/(home)/courses/page.tsx
+++ langsuit/app/(home)/courses/page.tsx
@@ -1,26 +1,7 @@
-// import { getCourses, getUserProgress } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
+
 import { List } from "./list";
 
-// Mocking database queries
-const getCourses = async () => {
-  return [
-    { id: 1, title: "English", description: "Learn English basics", imageSrc: "/US - United States.svg" },
-    { id: 2, title: "Spanish", description: "Master Spanish vocabulary", imageSrc: "/ES - Spain.svg" },
-    { id: 3, title: "French", description: "Explore French culture", imageSrc: "/FR - France.svg" },
-  ];
-};
-
-const getUserProgress = async () => {
-  return {
-    hearts: 15,
-    points: 900,
-    hasActiveSubscription: true,
-    activeCourseId: 2,
-    userName: "Akshat",
-  };
-};
-
-// CoursesPage Component
 const CoursesPage = async () => {
   const coursesData = getCourses();
   const userProgressData = getUserProgress();
@@ -31,12 +12,12 @@ const CoursesPage = async () => {
   ]);
 
   return (
-    <div className="h-full max-w-[912px] px-3 mx-auto">
-      <h1 className="text-2xl font-bold to-neutral-700">Courses Page</h1>
+    <div className="mx-auto h-full max-w-[912px] px-3">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+
       <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 };
 
 export default CoursesPage;
-
---
+++ langsuit/app/shop/page.tsx
@@ -0,0 +1,9 @@
+const ShopPage = () => {
+  return (
+    <div>
+      <h1>Sorry, Stripe Payment is Down Right Now. We'll Be Back Soon!!!</h1>
+    </div>
+  );
+}
+
+export default ShopPage;
