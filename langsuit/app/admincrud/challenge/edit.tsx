87974f42003758c60e9ca45c84b4551a96ab43e3
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-01-09 20:41:36 +0530

    [Refactor] Enhance challenge CRUD components with shared styles and improved layout

--- langsuit/app/admincrud/challenge/edit.tsx
+++ langsuit/app/admincrud/challenge/edit.tsx
@@ -7,28 +7,46 @@ import {
   TextInput,
   required,
 } from "react-admin";
+import { sharedStyles } from "../sharedStyles";
 
 export const ChallengeEdit = () => {
   return (
-    <Edit>
+    <Edit
+      sx={{
+        "& .RaEdit-main": sharedStyles.card,
+      }}
+    >
       <SimpleForm>
-        <TextInput source="question" validate={[required()]} label="Question" />
+        <TextInput
+          source="question"
+          validate={[required()]}
+          label="Question"
+          sx={sharedStyles.input}
+          fullWidth
+        />
         <SelectInput
           source="type"
           validate={[required()]}
           choices={[
-            {
-              id: "SELECT",
-              name: "SELECT",
-            },
-            {
-              id: "ASSIST",
-              name: "ASSIST",
-            },
+            { id: "SELECT", name: "SELECT" },
+            { id: "ASSIST", name: "ASSIST" },
           ]}
+          sx={sharedStyles.input}
+          fullWidth
         />
-        <ReferenceInput source="lessonId" reference="lessons" />
-        <NumberInput source="order" validate={required()} label="Order" />
+        <ReferenceInput
+          source="lessonId"
+          reference="lessons"
+          sx={sharedStyles.input}
+          fullWidth
+        />
+        <NumberInput
+          source="order"
+          validate={required()}
+          label="Order"
+          sx={sharedStyles.input}
+          fullWidth
+        />
       </SimpleForm>
     </Edit>
   );
--- langsuit/app/admincrud/challenge/list.tsx
+++ langsuit/app/admincrud/challenge/list.tsx
@@ -5,25 +5,33 @@ import {
   ReferenceField,
   SelectField,
   TextField,
+  TextInput,
 } from "react-admin";
+import { sharedStyles } from "../sharedStyles";
+
+const listFilters = [
+  <TextInput source="q" label="Search" alwaysOn sx={sharedStyles.input} />,
+];
 
 export const ChallengeList = () => {
   return (
-    <List>
-      <Datagrid rowClick="edit">
+    <List
+      filters={listFilters}
+      sx={{
+        "& .RaList-main": sharedStyles.card,
+      }}
+    >
+      <Datagrid
+        rowClick="edit"
+        sx={sharedStyles.table}
+      >
         <NumberField source="id" />
         <TextField source="question" />
         <SelectField
           source="type"
           choices={[
-            {
-              id: "SELECT",
-              name: "SELECT",
-            },
-            {
-              id: "ASSIST",
-              name: "ASSIST",
-            },
+            { id: "SELECT", name: "SELECT" },
+            { id: "ASSIST", name: "ASSIST" },
           ]}
         />
         <ReferenceField source="lessonId" reference="lessons" />
