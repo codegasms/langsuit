c386e0ed5a015d5ab8b5e45622015f86cb30c2f9
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-01-09 20:42:16 +0530

    [Refactor] Apply shared styles to unit create, edit, and list components

--- langsuit/app/admincrud/unit/create.tsx
+++ langsuit/app/admincrud/unit/create.tsx
@@ -6,19 +6,43 @@ import {
   TextInput,
   required,
 } from "react-admin";
+import { sharedStyles } from "../sharedStyles";
 
 export const UnitCreate = () => {
   return (
-    <Create>
+    <Create
+      sx={{
+        "& .RaCreate-main": sharedStyles.card,
+      }}
+    >
       <SimpleForm>
-        <TextInput source="title" validate={[required()]} label="Title" />
+        <TextInput 
+          source="title" 
+          validate={[required()]} 
+          label="Title"
+          sx={sharedStyles.input}
+          fullWidth
+        />
         <TextInput
           source="description"
           validate={[required()]}
           label="Description"
+          sx={sharedStyles.input}
+          fullWidth
         />
-        <ReferenceInput source="courseId" reference="courses" />
-        <NumberInput source="order" validate={required()} label="Order" />
+        <ReferenceInput 
+          source="courseId" 
+          reference="courses"
+          sx={sharedStyles.input}
+          fullWidth
+        />
+        <NumberInput 
+          source="order" 
+          validate={required()} 
+          label="Order"
+          sx={sharedStyles.input}
+          fullWidth
+        />
       </SimpleForm>
     </Create>
   );
--- langsuit/app/admincrud/unit/edit.tsx
+++ langsuit/app/admincrud/unit/edit.tsx
@@ -6,20 +6,50 @@ import {
   TextInput,
   required,
 } from "react-admin";
+import { sharedStyles } from "../sharedStyles";
 
 export const UnitEdit = () => {
   return (
-    <Edit>
+    <Edit
+      sx={{
+        "& .RaEdit-main": sharedStyles.card,
+      }}
+    >
       <SimpleForm>
-        <NumberInput source="id" validate={[required()]} label="Id" />
-        <TextInput source="title" validate={[required()]} label="Title" />
+        <NumberInput 
+          source="id" 
+          validate={[required()]} 
+          label="Id"
+          sx={sharedStyles.input}
+          fullWidth
+        />
+        <TextInput 
+          source="title" 
+          validate={[required()]} 
+          label="Title"
+          sx={sharedStyles.input}
+          fullWidth
+        />
         <TextInput
           source="description"
           validate={[required()]}
           label="Description"
+          sx={sharedStyles.input}
+          fullWidth
         />
-        <ReferenceInput source="courseId" reference="courses" />
-        <NumberInput source="order" validate={required()} label="Order" />
+        <ReferenceInput 
+          source="courseId" 
+          reference="courses"
+          sx={sharedStyles.input}
+          fullWidth
+        />
+        <NumberInput 
+          source="order" 
+          validate={required()} 
+          label="Order"
+          sx={sharedStyles.input}
+          fullWidth
+        />
       </SimpleForm>
     </Edit>
   );
