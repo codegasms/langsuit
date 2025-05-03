b53c7d20525b13f44c2b35b5fcacadcc51efbb87
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-05-03 20:53:38 +0530

    [Feat] [WIP] [Bishwajeet] update .gitignore and package.json for Jest test results and reporting

--- langsuit/jest.setup.js
+++ langsuit/jest.setup.js
@@ -4,3 +4,14 @@ jest.mock("react", () => ({
   ...jest.requireActual("react"),
   cache: (fn) => fn,
 }));
+
+// Mock Headers API
+global.Headers = class {
+  constructor(init) {
+    this.headers = init || {};
+  }
+
+  get(name) {
+    return this.headers[name] || null;
+  }
+};
